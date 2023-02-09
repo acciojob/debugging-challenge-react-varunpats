7007ebd5e77de325a228b6659b3b3fa7842c451d
varunpats <varunpatil2502@gmail.com>
2023-02-09 12:55:54 +0530

    Changes in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,13 @@ import React, { useEffect, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  let [count, setCount] = useState(0);
 
-  const alertMsg = () => {
-    alert("cant edit it");
-  }
-
-  const setCnt = () => {
-    setCount(count + 1);
-  }
+  let [count, setCount] = useState(0)
 
   return (
     <div className="ball">
-      <h1 className="count" onDoubleClick={alertMsg()}>{count}</h1>
-      <button className='increment-button' onClick={setCnt()}>Increment</button>
+      <h1 className="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
+      <button className='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
     </div>
   )
 }
